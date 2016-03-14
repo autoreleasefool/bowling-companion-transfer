9004822425c10988bfed5ced670d82b325cd9b37
Joseph Roque <contact@josephroque.ca>
2016-03-14 01:05:54 -0700

    Transfer data stored in mongodb

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ let formidable = require('formidable');
 let http = require('http');
 let fs = require('fs-extra');
 let cron = require('cron');
+let mongodb = require('mongodb');
 let secret = require('./secret');
 
-// Port for the server to run on
+// Port for the server to run on.
 const PORT = 8080;
 // Amount of time that a transfer will live on the server before being removed.
-const TRANSFER_TIME_TO_LIVE = 1000 * 60 * 60;
+const TRANSFER_TIME_TO_LIVE = 1000 * 15;
+// Directory to store user data.
+const USER_DATA_LOCATION = './test/';
 
 let indexHtml = null;
 try {
@@ -19,6 +22,13 @@ try {
   indexHtml = '<h1>Error loading page.</h1>';
   console.error(err);
 }
+
+// MongoDB
+let MongoClient = mongodb.MongoClient;
+// Local URL of the mongo database.
+const MONGO_URL = 'mongodb://localhost:27017/bowlingdata';
+// Name of the collection that data will be stored in.
+const MONGO_COLLECTION = 'transfers';
 
 // Generate an ID to represent the file uploaded.
 const POSSIBLE_ID_VALUES = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
@@ -31,8 +41,8 @@ let generateId = function() {
   return text;
 };
 
-// Dictionary of objects which contain location and upload time of user bowling data.
-let transferredData = {};
+// Dictionary of keys which have already been used as IDs for transfers.
+let usedKeys = {};
 
 // Job to remove files which have been around longer than 1 hour.
 // Runs once per hour.
@@ -41,28 +51,51 @@ let cleanupCronJob = new cron.CronJob({
   onTick: function() {
     console.log('Running cleanupCronJob on ' + (new Date()));
 
-    let currentTime = Date.now();
-    for (var key in transferredData) {
-      if (!transferredData.hasOwnProperty(key)) {
-        continue;
+    MongoClient.connect(MONGO_URL, function(err, db) {
+      if (err) {
+        console.error('Error establishing database connection.');
+        console.error(err);
+        return;
       }
 
-      let transferData = transferredData[key];
-      if (transferData.time + TRANSFER_TIME_TO_LIVE < currentTime) {
-        // This file has expired, so remove it.
-        fs.remove(transferData.location, function(err) {
-          if (err) {
-            console.error(`Error removing file ${transferData.location}`);
-            console.error(err);
-          } else {
-            console.log(`Succesfully deleted file ${transferData.location}`);
+      console.log('Established database connection.');
+
+      let currentTime = Date.now();
+
+      // Get a cursor to iterate over the collection.
+      let collection = db.collection(MONGO_COLLECTION);
+      let cursor = collection.find();
+
+      cursor.each(function(err, doc) {
+        if (err) {
+          console.error('Error retrieving documents.');
+          console.error(err);
+        } else if (doc !== null) {
+          if (doc.time + TRANSFER_TIME_TO_LIVE < currentTime) {
+            if (doc.key in usedKeys) {
+              // Free up the key to be used elsewhere.
+              delete usedKeys[doc.key];
+            }
+
+            // This file has expired so remove it.
+            fs.remove(doc.location, function(err) {
+              if (err) {
+                console.error(`Error removing file ${doc.location}`);
+                console.error(err);
+              } else {
+                console.log(`Succesfully deleted file ${doc.location}`);
+              }
+            });
+
+            // Delete the entry from the database
+            collection.deleteOne({key: doc.key});
+          } else if (!(doc.key in usedKeys)) {
+            // Key is still in use, but isn't in used keys for some reason.
+            usedKeys[doc.key] = true;
           }
-        });
-
-        // Delete this key, since it no longer points to a valid transfer, regardless of whether the file was removed.
-        delete transferredData[key];
-      }
-    }
+        }
+      });
+    });
   },
   start: false,
   timeZone: 'America/Los_Angeles'
@@ -86,8 +119,8 @@ let server = http.createServer(function(req, res) {
     let requestId = null;
     do {
       requestId = generateId();
-    } while (requestId in transferredData);
-    transferredData[requestId] = null;
+    } while (requestId in usedKeys);
+    usedKeys[requestId] = true;
     console.log(`Request ID: ${requestId}`);
 
     console.log('Initializing form.');
@@ -109,28 +142,19 @@ let server = http.createServer(function(req, res) {
       /* Temporary location of our uploaded file */
       var temporaryPath = this.openedFiles[0].path;
       /* The file name of the uploaded file */
-      var fileName = this.openedFiles[0].name;
+      var fileName = requestId;
 
       console.log(`Transfer complete. File location: ${temporaryPath}`);
 
-      // Location where we want to copy the uploaded file
-      var newLocation = './test/';
-
       // Copy the file to the new location.
-      fs.copy(temporaryPath, newLocation + fileName, function(err) {
+      fs.copy(temporaryPath, USER_DATA_LOCATION + fileName, function(err) {
         if (err) {
           console.error('Error copying file.');
           console.error(err);
         } else {
-          let fullName = newLocation + fileName;
+          let fullName = USER_DATA_LOCATION + fileName;
           console.log(`File copied successfully. File location: ${fullName}`);
         }
-
-        // Keep track of where the file is and when it was put there.
-        transferredData[requestId] = {
-          'time': Date.now(),
-          'location': newLocation + fileName,
-        };
 
         // Delete the old temp file
         fs.remove(temporaryPath, function(err) {
@@ -140,6 +164,29 @@ let server = http.createServer(function(req, res) {
           } else {
             console.log('Succesfully deleted temp file.');
           }
+        });
+
+        // Store the location of the data in the database.
+        MongoClient.connect(MONGO_URL, function(err, db) {
+          if (err) {
+            console.error('Error establishing database connection.');
+            console.error(err);
+            return;
+          }
+
+          let collection = db.collection(MONGO_COLLECTION);
+          collection.insert({
+            key: requestId,
+            time: Date.now(),
+            location: USER_DATA_LOCATION + fileName
+          }, function(err, records) {
+            if (err) {
+              console.error('Failed to insert record.');
+              console.error(err);
+            }
+
+            db.close();
+          });
         });
       });
     });
