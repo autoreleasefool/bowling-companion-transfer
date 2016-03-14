76f2eafa3489a9b0e226d0f55296a4ede0ce3a78
Joseph Roque <contact@josephroque.ca>
2016-03-13 22:09:44 -0700

    Added task to clean up transfer data each hour

--- server.js
+++ server.js
@@ -4,10 +4,13 @@
 let formidable = require('formidable');
 let http = require('http');
 let fs = require('fs-extra');
+let cron = require('cron');
 let secret = require('./secret');
 
 // Port for the server to run on
 const PORT = 8080;
+// Amount of time that a transfer will live on the server before being removed.
+const TRANSFER_TIME_TO_LIVE = 1000 * 60 * 60;
 
 let indexHtml = null;
 try {
@@ -18,15 +21,52 @@ try {
 }
 
 // Generate an ID to represent the file uploaded.
-const POSSIBLE_ID_VALIES = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const POSSIBLE_ID_VALUES = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 let generateId = function() {
   let text = '';
 
   for (var i = 0; i < 5; i++)
-    text += POSSIBLE_ID_VALIES.charAt(Math.floor(Math.random() * POSSIBLE_ID_VALIES.length));
+    text += POSSIBLE_ID_VALUES.charAt(Math.floor(Math.random() * POSSIBLE_ID_VALUES.length));
 
   return text;
 };
+
+// Dictionary of objects which contain location and upload time of user bowling data.
+let transferredData = {};
+
+// Job to remove files which have been around longer than 1 hour.
+let cleanupCronJob = new cron.CronJob({
+  cronTime: '0 0 * * * *',
+  onTick: function() {
+    console.log('Running cleanupCronJob at ' + (new Date()));
+
+    let currentTime = Date.now();
+    for (var key in transferredData) {
+      if (!transferredData.hasOwnProperty(key)) {
+        continue;
+      }
+
+      let transferData = transferredData[key];
+      if (transferData.time + TRANSFER_TIME_TO_LIVE < currentTime) {
+        // This file has expired, so remove it.
+        fs.remove(transferData.location, function(err) {
+          if (err) {
+            console.error(`Error removing file ${transferData.location}`);
+            console.error(err);
+          } else {
+            console.log(`Succesfully deleted file ${transferData.location}`);
+          }
+        });
+
+        // Delete this key, since it no longer points to a valid transfer, regardless of whether the file was removed.
+        delete transferredData[key];
+      }
+    }
+  },
+  start: false,
+  timeZone: 'America/Los_Angeles'
+});
+cleanupCronJob.start();
 
 let server = http.createServer(function(req, res) {
 
@@ -40,6 +80,14 @@ let server = http.createServer(function(req, res) {
       res.end();
       return;
     }
+
+    // Generate a unique ID for this request.
+    let requestId = null;
+    do {
+      requestId = generateId();
+    } while (requestId in transferredData);
+    transferredData[requestId] = null;
+    console.log(`Request ID: ${requestId}`);
 
     console.log('Initializing form.');
     let form = new formidable.IncomingForm();
@@ -57,28 +105,34 @@ let server = http.createServer(function(req, res) {
     });
 
     form.on('end', function(fields, files) {
-
       /* Temporary location of our uploaded file */
-      var temp_path = this.openedFiles[0].path;
+      var temporaryPath = this.openedFiles[0].path;
       /* The file name of the uploaded file */
-      var file_name = this.openedFiles[0].name;
+      var fileName = this.openedFiles[0].name;
 
-      console.log('Transfer complete. File location: ' + temp_path);
+      console.log(`Transfer complete. File location: ${temporaryPath}`);
 
       // Location where we want to copy the uploaded file
-      var new_location = './test';
+      var newLocation = './test';
 
       // Copy the file to the new location.
-      fs.copy(temp_path, new_location + file_name, function(err) {
+      fs.copy(temporaryPath, newLocation + fileName, function(err) {
         if (err) {
           console.error('Error copying file.');
           console.error(err);
         } else {
-          console.log('File copied successfully. File location: ' + new_location + file_name);
+          let fullName = newLocation + fileName;
+          console.log(`File copied successfully. File location: ${fullName}`);
         }
 
+        // Keep track of where the file is and when it was put there.
+        transferredData[requestId] = {
+          'time': Date.now(),
+          'location': newLocation + fileName,
+        };
+
         // Delete the old temp file
-        fs.remove(temp_path, function(err) {
+        fs.remove(temporaryPath, function(err) {
           if (err) {
             console.error('Error removing temp file.');
             console.error(err);
